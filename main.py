2638732c13924f6ae0fbe635b0cee87929dfa372
Ryo Ogura <ryo@MacBook-Pro-6.local>
2024-11-01 02:13:04 +0900

    get(),calculate()の実装

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/calculate/{sum_element1}/{sum_element2}")
+async def calculate(sum_element1:int,sum_element2:int):
+    sum_result=sum_element1+sum_element2
+    return sum_result
+
+@app.post("/calculate/sum")
+async def calculate(sum_element1:int,sum_element2:int):
+    sum_result=sum_element1+sum_element2
+    return sum_result
+
